4d1cc19cee60ac542b6090528678b80659b17aa7
Addy897 <145797684+Addy897@users.noreply.github.com>
2025-09-08 08:42:32 +0530

    Login/Team

--- src/components/validate.tsx
+++ src/components/validate.tsx
@@ -1,29 +1,24 @@
 'use server'
 import { StudentData } from "@/data/branches";
-import { createStudentData, isAlreadyUsed} from "./db_functions";
+import { createStudentData, isAlreadyUsed } from "./db_functions";
 
 export async function val(data: StudentData) {
   const bitMail = /^1bi\d{2}[a-zA-Z]{2}\d{3}@bit-bangalore\.edu\.in$/
   if (data.collegeEmail && bitMail.test(data.collegeEmail)) {
-      const isUsed = await isAlreadyUsed(data);
-     
-   if(isUsed){
-      
+    const isUsed = await isAlreadyUsed(data);
+
+    if (isUsed) {
+
       return {
-          result: false,
-          message: "College Email Already Used."
-        };
+        result: false,
+        message: "College Email Already Used."
+      };
     }
-       else{ 
-          return await createStudentData(data,"https://u.payu.in/PAYUMN/RJG7WHQDe7UP");      
+    else {
+      return await createStudentData(data, "https://u.payu.in/PAYUMN/RJG7WHQDe7UP");
     }
   } else {
-    console.log(data.collegeEmail)
-    return {
-      result: false,
-      message: "College Email REGEX FAILED"
-    };
-
+    return await createStudentData(data, "https://u.payu.in/PAYUMN/iri6CRZ5sokw");
   }
 
 
